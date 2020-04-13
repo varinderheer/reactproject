5e625246cbd90d816b30f8f6be812977e5948dc3
varinderheer8 <virenkumar.arni@gmail.com>
2020-04-13 17:14:08 +0530

    modals ka bhi pagal yaad raaakh

---
+++ reset/resets/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ResetsConfig(AppConfig):
+    name = 'resets'
---
+++ reset/resets/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate,login
+from django.shortcuts import render
+from .form import LoginForm
+from django.http import HttpResponse
+
+
